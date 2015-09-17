5271db4d0e1cf94980d673c648285cd31a7bb4c2
Sam Eubank <sameubank+zerious@gmail.com>
2015-09-17 08:23:55 -0700

    Initial commit

---
+++ test/flagger-bench.js
@@ -0,0 +1,16 @@
+var fs = require('fs')
+var flagger = require(__dirname + '/lighter-flagger')
+
+describe('Benchmarks', function () {
+
+  bench('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
---
+++ test/flagger-test.js
@@ -0,0 +1,26 @@
+var flagger = require('../lighter-flagger')
+var is = global.is || require('exam/lib/is')
+
+describe('flagger', function () {
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
